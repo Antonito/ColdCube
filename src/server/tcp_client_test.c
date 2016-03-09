d1351b0d6ce0daaebe42de0653d44e0a4876bef2
Lucas Troncy <lucas.troncy@epitech.eu>
2016-03-09 14:35:17 +0000

    stress Test in progress

--- src/server/tcp_client_test.c
+++ src/server/tcp_client_test.c
@@ -3,22 +3,46 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <pthread.h>
+#include <string.h>
 
-int		main()
+int		run = 42;
+
+void		*tcp_thread(void *data)
+{
+  char		buff[200];
+  int		len;
+  int		tsock = *((int *) data);
+
+  printf("socked WHILE is %d\n", tsock);
+  exit(0);
+  while (run)
+    {
+      len = read(tsock, buff, 199);
+      buff[len] = 0;
+      fprintf(stdout, "%s\n", buff);
+    }
+}
+
+int		main(int argc, char **argv)
 {
   struct sockaddr_in server;
   int		sock;
-  int		len;
-  char		buff[2048];
   int		len2;
   char		buff2[200];
+  pthread_t	ttcp;
 
+  if (argc < 2)
+    {
+      fprintf(stderr, "please enter an ip address\n");
+	return (1);
+    }
   if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == -1)
     {
       fprintf(stderr, "socket creation failed\n");
       return (1);
     }
-  server.sin_addr.s_addr = inet_addr("127.0.0.1");
+  server.sin_addr.s_addr = inet_addr(argv[1]);
   server.sin_family = AF_INET;
   server.sin_port = htons(12345);
   if (connect(sock, (struct sockaddr *)&server, sizeof(server)) < 0)
@@ -31,24 +55,12 @@ int		main()
       fprintf(stderr, "error sending MSG\n");
       return (1);
     }
-  if ((len = read(sock, buff, 1024)) == 0)
-    {
-      fprintf(stderr, "server closed stream :(\n");
-      return (1);
-    }
-  buff[len] = 0;
-  fprintf(stdout, "recv : %s\n", buff);
+  pthread_create(&ttcp, NULL, tcp_thread, (void *)&sock);
   while ((len2 = read(0, buff2, 199)) > 0)
     {
       buff2[len2] = 0;
       write(sock, buff2, strlen(buff2));
-      if ((len = read(sock, buff, 1024)) == 0)
-	{
-          fprintf(stderr, "server closed stream :(\n");
-         return (1);
-	}
-      buff[len] = 0;
-      fprintf(stdout, "recv : %s\n", buff);
     }
+  run = 0;
   return (0);
 }
