7d45b85054492fae42d6719a75fb6045525d659c
Lucas Troncy <lucas.troncy@epitech.eu>
2016-03-11 11:11:59 +0000

    stress Test in progress6

--- src/server/udp/server_udp_msg.c
+++ src/server/udp/server_udp_msg.c
@@ -3,11 +3,14 @@
 void		udps_send_to_all(t_udps *udp)
 {
   int		i;
+  int		j;
 
   i = -1;
+  j = -1;
   while (++i < udp->nb_actual)
     {
-      sendto(udp->main_sock, udp->cli_buff[i], 70, 0, (struct sockaddr *)&udp->cli_sock[i], udp->cli_addrl);
+      while (++j < udp->nb_actual)
+	sendto(udp->main_sock, udp->cli_buff[j], 70, 0, (struct sockaddr *)&udp->cli_sock[i], udp->cli_addrl);
     }
 }
 
