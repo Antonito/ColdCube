f45e495bcf9ee5172d8a760f2b51c33e35f8566e
boulag_l <boulag_l@epitech.eu>
2016-03-29 18:08:47 +0200

    Adding Menu

---
+++ menu/Menu.cpp
@@ -0,0 +1,65 @@
+#include "SDL2/SDL.h"
+#include "SDL2/SDL_ttf.h"
+#include "Menu.h"
+#include <vector>
+#include <string>
+
+Menu::Menu(SDL_Surface *screen, std::vector<menuItem> *items) {
+  this->items = items;
+  this->screen = screen;
+  this->currentItem = 0;
+  TTF_Init();
+  this->font = TTF_OpenFont("tata.ttf", 12);
+  printf("Font = %p %s\n", this->font, TTF_GetError());
+}
+
+Menu::~Menu() {
+  SDL_FreeSurface(this->textSurface);
+  TTF_CloseFont(this->font);
+  TTF_Quit();
+}
+
+void Menu::hover(int x, int y) {
+}
+
+void Menu::moveDown() {
+  if (this->currentItem < (this->items->size() - 1))
+    ++this->currentItem;
+}
+
+void Menu::moveUp() {
+  if (this->currentItem)
+    --this->currentItem;
+}
+
+void Menu::draw() {
+  SDL_Rect positionZozor;
+  SDL_Surface *zozor = SDL_LoadBMP("zozor.bmp");
+  SDL_Surface *text;
+  SDL_Surface *bg = SDL_CreateRGBSurface(SDL_SWSURFACE, 300, 70, 32, 0, 0, 0, 0);
+  SDL_Color color;
+
+  color.r = 255;
+  color.g = 0;
+  color.b = 0;
+  positionZozor.x = 10;
+  positionZozor.y = 10;
+  int i = this->items->size();
+  printf("Drawing menu... %d\n", i);
+  while (i--) {
+    SDL_SetColorKey(zozor, 0, SDL_MapRGB(zozor->format, 0, 0, 255));
+    SDL_SetColorKey(bg, 0, SDL_MapRGB(bg->format, 0, 0, 255));
+    SDL_FillRect(zozor, NULL, SDL_MapRGB(zozor->format, 255, 255, 0));
+    SDL_FillRect(bg, NULL, SDL_MapRGB(bg->format, 255, 255, 0));
+    text = TTF_RenderText_Blended(this->font, (*this->items)[this->items->size() - i - 1].text.c_str(), color);
+    SDL_BlitSurface(bg, NULL, this->screen, &positionZozor);
+    SDL_BlitSurface(text, NULL, this->screen, &positionZozor);
+    positionZozor.y += 80;
+  }
+  SDL_FreeSurface(zozor);
+  SDL_FreeSurface(bg);
+}
+
+void Menu::selectCurrentItem() {
+  printf("Selecting item %d\n", this->currentItem);
+}
---
+++ menu/Menu.h
@@ -0,0 +1,37 @@
+#include "SDL/SDL.h"
+#include "SDL/SDL_ttf.h"
+#include <vector>
+#include <string>
+
+typedef struct	Point
+{
+  int	x;
+  int	y;
+}		Point;
+
+struct menuItem
+{
+	std::string text;
+	Point box[4];
+	void (*ptToFunc)();
+};
+
+class Menu
+{
+public:
+	Menu(SDL_Surface *, std::vector<menuItem> *);
+	~Menu();
+	void hover(int, int);
+	void moveUp();
+	void moveDown();
+	void draw();
+	void selectCurrentItem();
+	int currentItem;
+private:
+	TTF_Font *font;
+	SDL_Surface *screen;
+	Point menuLocation;
+	std::vector<menuItem> *items;
+	SDL_Surface *textSurface;
+	SDL_Rect cursor;
+};
