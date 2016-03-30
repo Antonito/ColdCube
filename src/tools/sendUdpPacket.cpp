fe24e05c984407d2ad7badf30f26a24a1fcbaff6
Ludovic Petrenko <ludovic.petrenko@epitech.eu>
2016-03-30 07:23:28 +0200

    test
    Former-commit-id: 5a4594a4521142f42ee0ac2a5a7a825285c91a04

--- src/tools/sendUdpPacket.cpp
+++ src/tools/sendUdpPacket.cpp
@@ -1,10 +1,16 @@
 #include "common_structs.hpp"
+#include <stdio.h>
+#include <unistd.h>
 
 int		sendUdpPacket(t_data *data, char packet[70])
 {
   unsigned int	len;
 
   len = sizeof(data->net.udp.to_serv);
+
+  printf("sock %d\n", data->net.udp.sock);
+  printf("%p\n", packet);
+  write(1, packet, 70);
   if (sendto(data->net.udp.sock, packet, 70, 0,
                  (struct sockaddr *)&data->net.udp.to_serv, len) == -1)
     return (1);
