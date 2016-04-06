9e96d548539ad3bbb8d6c8c10194144840a233de
Antoine Baché <antoine.bache@epitech.eu>
2016-04-06 02:42:58 +0200

    Usleep serv

--- src/tools/udp_client.cpp
+++ src/tools/udp_client.cpp
@@ -24,7 +24,7 @@ void			*udp_send_thread(void *data)
   while (_data->net.udp.run_send)
     {
       gettimeofday(&t1, NULL);
-      if (t1.tv_usec % 15000 <= 100)
+      if (t1.tv_usec % 30000 <= 1000)
 	createUdpPacket(_data, &_data->players[_data->net.playerIndexUdp]);
     }
   return (NULL);
