af70557a407cdd258decc70b9cb0b8bc0fbf4ce2
Antoine Baché <antoine.bache@epitech.eu>
2016-04-07 15:28:26 +0200

    Cleaned code

--- src/tools/events.cpp
+++ src/tools/events.cpp
@@ -1,29 +1,10 @@
 #include "common_structs.hpp"
 #include <iostream>
 
-void printBits(size_t const size, void const * const ptr)
-{
-  unsigned char *b = (unsigned char*) ptr;
-  unsigned char byte;
-  int i, j;
-
-  for (i=size-1;i>=0;i--)
-    {
-      for (j=7;j>=0;j--)
-	{
-	  byte = b[i] & (1<<j);
-	  byte >>= j;
-	  printf("%u", byte);
-	}
-    }
-  puts("");
-}
-
 void		setEvent(uint32_t *value, int event, bool stat)
 {
   t_uevent	set;
 
-  printBits(sizeof(uint32_t), value);
   set.value = *value;
   if (event == CONNECTED)
     set.event.connected = stat;
@@ -34,7 +15,6 @@ void		setEvent(uint32_t *value, int event, bool stat)
     std::cerr << "Event doesn't exist\n";
 #endif
   *value = set.value;
-  printBits(sizeof(uint32_t), value);
 }
 
 bool		getEvent(uint32_t value, int event)
