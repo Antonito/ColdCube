47a79c7af026c5ce8635462cf5c35df31907fef1
Antoine Baché <antoine.bache@epitech.eu>
2016-04-12 00:18:44 +0200

    Changed damages

--- src/game/damage.cpp
+++ src/game/damage.cpp
@@ -2,6 +2,7 @@
 
 unsigned char	getDamage(int weapon, int part, double dist)
 {
+  unsigned char	tmp;
   switch(weapon)
     {
     case (RIFLE_WEAPON):
@@ -9,13 +10,13 @@ unsigned char	getDamage(int weapon, int part, double dist)
 	switch(part)
 	  {
 	  case (HEAD_HIT):
-	    return (60 / dist * 4);
+	    return (((tmp = 60 / dist * 4) > 30) ? tmp : 30);
 	  case (BODY_HIT):
-	    return (25 / dist * 4);
+	    return (((tmp = 25 / dist * 4) > 12) ? tmp : 12);
 	  case (ARM_HIT):
-	    return (5 / dist * 4);
+	    return (((tmp = 5 / dist * 4)  > 2) ? tmp : 2);
 	  case (LEG_HIT):
-	    return (15 / dist * 4);
+	    return (((tmp = 15 / dist * 4)  > 7) ? tmp : 7);
 	  default:
 	    return (0);
 	  }
@@ -25,20 +26,20 @@ unsigned char	getDamage(int weapon, int part, double dist)
 	switch(part)
 	  {
 	  case (HEAD_HIT):
-	    return (80 / dist) * 4;
+	    return ((((tmp = 80 / dist) * 4) > 40) ? tmp : 40);
 	  case (BODY_HIT):
-	    return (30 / dist * 4);
+	    return (((tmp = 30 / dist * 4) > 15) ? tmp : 15);
 	  case (ARM_HIT):
-	    return (20 / dist * 4);
+	    return (((tmp = 20 / dist * 4) > 10) ? tmp : 10);
 	  case (LEG_HIT):
-	    return (5 / dist * 4);
+	    return (((tmp = 5 / dist * 4) > 2) ? tmp : 2);
 	  default:
 	    return (0);
 	  }
       }
     case (KNIFE_WEAPON):
       {
-	if (dist >= 0.8)
+	if (dist >= 1.2)
 	  return (0);
 	switch(part)
 	  {
